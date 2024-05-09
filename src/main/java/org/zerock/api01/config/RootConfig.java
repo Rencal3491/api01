bd371b164aecc1893acd04995b0e5b1496d91d6e
MJMM <jmj3491329@naver.com>
2024-05-09 11:14:45 +0900

    asd

--- src/main/java/org/zerock/api01/config/RootConfig.java
+++ src/main/java/org/zerock/api01/config/RootConfig.java
@@ -1,11 +1,13 @@
-package org.zerock.b01.config;
+package org.zerock.api01.config;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
+@EnableJpaAuditing
 public class RootConfig {
 
     @Bean
@@ -14,7 +16,7 @@ public class RootConfig {
         modelMapper.getConfiguration()
                 .setFieldMatchingEnabled(true)
                 .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
-                .setMatchingStrategy(MatchingStrategies.STRICT);
+                .setMatchingStrategy(MatchingStrategies.LOOSE);
         return modelMapper;
     }
 
--- src/main/java/org/zerock/api01/config/SwaggerConfig.java
+++ src/main/java/org/zerock/api01/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package org.zerock.b01.config;
+package org.zerock.api01.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
