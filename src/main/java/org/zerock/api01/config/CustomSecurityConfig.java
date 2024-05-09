d63624170dc7a977f28af27acc55161a03deb415
MJMM <jmj3491329@naver.com>
2024-05-09 12:32:25 +0900

    0506 4교시

--- src/main/java/org/zerock/api01/config/CustomSecurityConfig.java
+++ src/main/java/org/zerock/api01/config/CustomSecurityConfig.java
@@ -5,6 +5,8 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -12,12 +14,17 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.zerock.api01.security.APIUserDetailService;
 
 @Configuration
 @Log4j2
 @RequiredArgsConstructor
 @EnableMethodSecurity
 public class CustomSecurityConfig {
+
+    //의존성 주입 - 실제 인증처리를 위한 AuthenticationManager 객체 설정 필요
+    private final APIUserDetailService apiUserDetailService;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -26,16 +33,32 @@ public class CustomSecurityConfig {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         log.info("-------------------- web configure  -------------------");
+
         // 정적 리소스 필터링 제외~~~
         return (web -> web.ignoring()
                 .requestMatchers(
                         PathRequest.toStaticResources().atCommonLocations())
                 );
+
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         log.info("-------------------configure------------------------");
+
+        //AuthenticationManager 설정
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        authenticationManagerBuilder
+                .userDetailsService(apiUserDetailService)
+                        .passwordEncoder(passwordEncoder());
+
+        //Get AuthenticationManager
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
+        //인증 매니저 등록
+        http.authenticationManager(authenticationManager);
 
         // CSRF 토큰 비활성화
         http.csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable());
